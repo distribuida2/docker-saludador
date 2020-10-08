958e62fe292d8cffc46fab7012d96f0d8215d752
Juan Lagostena <jplagostena@undav.edu.ar>
2020-10-08 00:01:46 -0300

    Incluimos el README y cómo empezar el ejercicio

---
+++ README.md
@@ -0,0 +1,28 @@
+# Docker Saludador
+
+## Ya nos vimos antes
+
+Exacto! Este *saludador* es el proyecto que ya vimos como *docker-segundo-ejemplo*, solo que ahora lo tomaremos como punto de partida
+
+### Construyamos la imágen y corramos un contenedor
+
+Corremos el comando de abajo para construir la imágen y el -t indica que tag le queremos poner.
+
+`docker build -t saludador:0.1 .`
+
+Una vez que la imágen ya está construida, instanciamos un container de esa imágen.
+
+`docker run -v $PWD:/app -p 80:80 --name saludador saludador:0.1:latest`
+
+#### Preguntas
+
+- ¿Qué es ese -v?
+- ¿Y el -p?
+
+### Referencias
+
+- [Dockerfile reference](https://docs.docker.com/engine/reference/builder/)
+
+### Hecho con ❤️amor❤️ para
+
+Programación Distribuida II / Universidad Nacional de Avellaneda
--- saludador.php
+++ saludador.php
@@ -1,26 +1,90 @@
 <?php
+    session_start();
+
     $ip = $_SERVER['SERVER_ADDR'];
+
+    $count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
+    $_SESSION['count'] = $count + 1;
+
+    $show_headers = getenv("DEBUG_HEADERS");
+    $date = date("d/m/Y h:i");
+
 ?>
 <html>
 <head>
-<title>Saludador - UNDAV - Programación Distribuida II - <?= $ip ?></title>
-<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+<title>Saludador - UNDAV - Programación Distribuida II</title>
+<link rel="shortcut icon" href="https://www.undav.edu.ar/general/recursos/adjuntos/1441.ico">
+<style>
+    body {
+        font-family: arial, verdana, sans-serif;
+        font-size: 12pt;
+        background-color: #cacaca ;
+        margin: auto;
+        width: 1003px;
+        height: 132px;
+    }
+    
+    #header {
+        width: 1003px;
+        height: 132px;
+        margin: auto;
+        background: #2a3665 url("https://undav.edu.ar/general/recursos/adjuntos/25822.jpg") no-repeat;
+        position: relative;
+        padding: 5px;       
+    }
+
+    #contenido {
+        width: 1003px;
+        margin: auto;
+        padding: 5px;
+        overflow: hidden;
+        background: #fff;
+    }
+
+    .tile {
+        padding: 15px;
+        font-size: 12pt; 
+    }
+
+    .tile div.data {
+        font-weight: bold;
+        color: green;
+        font-size: 15pt;
+    }   
+</style>
 </head>
 <body>
-<?php
-
-echo "Hola!<br><br>Hoy es: ".date("d/m/Y h:i");
-
-echo "<br>";
-echo "Server IP: $ip<br>";
-echo "<br>Headers<br><br>";
-
-foreach (getallheaders() as $nombre => $valor) {
-    echo "$nombre: $valor<br>";
-}
-
-?>
-
+<div id="header"></div>
+<div id="contenido">
+    <h3>Hola terrícola!</h3>
+    <div id="intro">
+        <p>Esto es una simple web hecha con amor <span style="color: red">❤️</span> en PHP para ver un par de conceptos de contenedores y escalabilidad horizontal de la materia Programación Distribuida II.</p>
+    
+        <p>Podés encontrar su código en este <a href="https://github.com/distribuida2/docker-saludador">repositorio</a></p>
+    </div>
+    <h3>Info</h3>
+    <div id="info">
+        <div class="tile">
+            Día y hora
+            <div class="data"><?= $date ?></div>
+        </div>
+        <div class="tile">
+            La ip del web server es 
+            <div class="data"><?= $ip ?></div>
+        </div>
+        <div class="tile">La cantidad de recargas es 
+            <div class="data"><?= $count ?></div    >
+        </div>
+        <?php
+        if ($show_headers) {
+            echo "<br><br>";
+            foreach (getallheaders() as $nombre => $valor) {
+                echo "$nombre: $valor<br>";
+            }
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html>
 
